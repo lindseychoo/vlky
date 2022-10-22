b9d7f19180adff5f848043a25cffa2a0477255c3
lindsey <choolindsey@gmail.com>
2022-10-22 10:08:11 +0800

    Initial Commit

---
+++ vlky/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  vlky
+//
+//  Created by Lindsey Choo on 22/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
