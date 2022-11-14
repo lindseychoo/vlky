7f72127b7f4f7001315cbaa490389a90d475c3cb
Tan Yun Jun <yunjun09@rafflesgirlssch.edu.sg>
2022-11-14 13:05:34 +0800

    store title added

--- vlky/StoreView.swift
+++ vlky/StoreView.swift
@@ -10,28 +10,37 @@ import SwiftUI
 struct StoreView: View {
     
     var items = [storeItem(itemName: "Chest 1", imageName: "chest1"), storeItem(itemName: "Chest 2", imageName: "chest2"), storeItem(itemName: "Chest 3", imageName: "chest3"), storeItem(itemName: "Chest 4", imageName: "chest4")]
-    let columns = [GridItem(.adaptive(minimum: 160))]
+    let columns = [GridItem(.adaptive(minimum: 120))]
     
     var body: some View {
         
-        LazyVGrid(columns: columns) {
-            ForEach(items) { item in
-                Button {
-                    
-                } label: {
-                    VStack {
-                        Image(item.imageName)
-                        Text(item.itemName)
-                            .font(.system(size: 30))
-                            .foregroundColor(.white)
+        
+        NavigationView {
+            VStack{
+                LazyVGrid(columns: columns) {
+                    ForEach(items) { item in
+                        Button {
+                            print("hi")
+                        } label: {
+                            VStack {
+                                Image(item.imageName)
+                                Text(item.itemName)
+                                    .font(.system(size: 30))
+                                    .foregroundColor(.white)
+                            }
+                            .padding(20)
+                            .background(.cyan)
+                            .cornerRadius(10)
+                        }
                     }
-                    .padding(20)
-                    .background(.cyan)
-                    .cornerRadius(10)
                 }
-            }
+                .padding()
+            }.navigationTitle("Store")
+                
+            
+
         }
-        .padding()
+        
     }
 }
 
