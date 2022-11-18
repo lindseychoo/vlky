2c316dab7a76d102da43c29ebe1de3047552c6a9
Tan Yun Jun <yunjun09@rafflesgirlssch.edu.sg>
2022-11-18 13:47:18 +0800

    added wallpaper to home view made extra minor changes

--- vlky/HomeView.swift
+++ vlky/HomeView.swift
@@ -18,61 +18,71 @@ struct HomeView: View {
     @State var currentEncouragement = 0
     
     var body: some View {
-        
-        VStack {
-            HStack {
-                Text("Welcome")
-                    .font(.system(size: 35))
-                    .padding()
-                Image(systemName: "dollarsign.circle")
-                    .font(.system(size: 35))
-                Text("\(numOfCoins)")
-                    .font(.system(size: 35))
-                
-            }
-            Image("mascot")
+        ZStack{
+            Image("HomeWallpaper")
                 .resizable()
                 .scaledToFit()
-                .mask(Circle())
-                .padding()
-       
-            ZStack {
-                Color(.systemBackground)
-                    .frame(maxHeight: 200)
-                .onTapGesture{
-                    if showEncouragement {
-                        currentEncouragement += 1
-                        showEncouragement = false
-                    }
-                }
-                VStack {
-                    Text(encouragements[currentEncouragement % encouragements.count].setup)
-                    
-                    Button {
-                        showEncouragement = true
-                    } label: {
-                        Text (" Valkey ")
-                            .font(.system(size: 35))
-                            .padding()
-                            .background(.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(20)
-                    }
-                    .padding()
-                    
-                    if showEncouragement {
-                        Text(encouragements [currentEncouragement % encouragements.count].words)
-                            .padding()
-                    }
-                    
-                }
-            }
-            }
-           
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea([.top, .bottom])
             
-        }
+            VStack {
+                HStack {
+                    Text("Welcome")
+                        .font(.system(size: 35))
+                        .padding()
+                    Image(systemName: "dollarsign.circle")
+                        .font(.system(size: 35))
+                    Text("\(numOfCoins)")
+                        .font(.system(size: 35))
+                    
+                }
+                Image("mascot")
+                    .resizable()
+                    .scaledToFit()
+                    .mask(Circle())
+                    .padding()
+                
+                ZStack {
+//                    Color(.systemBackground)
+//                        .frame(maxHeight: 200)
+//                        .onTapGesture{
+//                            if showEncouragement {
+//                                currentEncouragement += 1
+//                                showEncouragement = false
+//                            }
+//                        }
+                    VStack {
+                        Text(encouragements[currentEncouragement % encouragements.count].setup)
+                        
+                        Button {
+                            showEncouragement = true
+                            currentEncouragement += 1
+                        } label: {
+                            Text (" Valkey ")
+                                .font(.system(size: 35))
+                                .padding()
+                                .background(.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(20)
+                        }
+                        .padding()
+                        
+//                        if showEncouragement {
+                        Text(showEncouragement ? encouragements [currentEncouragement % encouragements.count].words : "")
+                            .font(.system(size: 25))
+                                .padding()
+//                        }
+                        
+                    }
+                }
+            }
         }
         
+        
+        
+    }
+}
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
